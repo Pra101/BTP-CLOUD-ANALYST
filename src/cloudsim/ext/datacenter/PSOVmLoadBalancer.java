919709af2bd60ed04d23a1f1918c570ab135fc72
Pra101 <Prahaladsingh271200@gmail.com>
2022-12-10 15:57:23 +0530

    first commit

---
+++ src/cloudsim/ext/datacenter/PSOVmLoadBalancer.java
@@ -0,0 +1,11 @@
+package cloudsim.ext.datacenter;
+
+public class PSOVmLoadBalancer extends VmLoadBalancer {
+    public PSOVmLoadBalancer(DatacenterController datacenterController) {
+    }
+
+    @Override
+    public int getNextAvailableVm() {
+        return 0;
+    }
+}
